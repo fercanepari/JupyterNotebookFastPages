33e322e6f40638cf9b65ea107c8ced2a0ee08efb
fercanepari <fercanepari@gmail.com>
2021-08-24 21:37:53 -0700

    streamlit added

---
+++ StreamLit/FirstPage.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 25 01:27:23 2021
+
+@author: CanepariF
+"""
+
+import streamlit as st
+
+st.write("""
+         # Test Título
+         
+         Test Mensaje
+         
+         """)
