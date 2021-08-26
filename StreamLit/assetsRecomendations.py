eb03a78e0373506dd49d0e9299cc2b31d86f5c3f
fercanepari <fercanepari@users.noreply.github.com>
2021-08-26 04:45:32 +0000

    deploy: 2310695f9606b71846e165c3b3b7d7871e9a145d

--- StreamLit/assetsRecomendations.py
+++ StreamLit/assetsRecomendations.py
@@ -72,58 +72,78 @@ counter = 1
 
 totalfields = len(tickers)
 
-print("Processing assets to find buy signals")
-st.title("Processing assets to find buy signals")
+st.title("Find stock oportunities to buy, among S&P 500 companies")
 
-progress_bar = st.progress(0)
+st.text("Using Relative Strength Index - RSI We can find signals to buy an asset across time")
+st.text("The following algorithm fetches all the tickers and if there is a signal to buy")
+st.text("in the recent days, it is displayed. ")
+st.text("For the found signal, we plot a chart with the asset close values across time, and all  ")
+st.text("the buy signals obtained with RSI strategy.")
 
-status_text = st.empty()
+st.text("The list of all tickers is obtained from wikipedia (https://en.wikipedia.org/wiki/List_of_S%26P_500_companies)")
 
-for i in range(totalfields):  
-    # Update progress bar.
-    progess = (i * 100 / totalfields) / 100
-    print(progess)
-    progress_bar.progress(progess)
-    
-    frame = RSIcalc(tickers[i])
-    buy, sell = getSignals(frame)
-    Profits = (frame.loc[sell].Open.values - frame.loc[buy].Open.values)/frame.loc[buy].Open.values
-    matrixsignals.append(buy)
-    matrixprofits.append(Profits)
-    
-    #Date range to check if there is a signal to buy the asset
-    dateTo = datetime.datetime.today()
-    dateFrom = dateTo + datetime.timedelta(days=-30)
-    
-    dfBuy = frame.loc[dateFrom:dateTo, ['Buy']]
-    toBuy = dfBuy[dfBuy['Buy']=='Yes']
-    toBuy['Ticker'] = tickers[i]
-    
-    print('Processed: ' + str(counter) + ' of ' + str(totalfields))
-    #st.title('Processed: ' + str(counter) + ' of ' + str(totalfields))
-    counter = counter + 1
-    
-    # Update status text.
-    status_text.text(
-        'Processed: ' + str(counter) + ' of ' + str(totalfields))
-    
-    #In case we have a signal to buy in the specified period, let's plot it
-    if not toBuy.empty:
-        print('Asset to buy: ')
-        
-        plt.figure(figsize=(12,5))
-        plt.scatter(frame.loc[buy].index, frame.loc[buy]['Adj Close'], marker='^', c='g')
-        plt.plot(frame['Adj Close'], alpha=0.7)
-        plt.title(label=tickers[i])
-        
-        st.write(toBuy)
-        st.line_chart(frame['Adj Close'])
+st.text("To start the process press the button...")
 
-        #st.plotly_chart(plt)
+result = st.button("Start Process")
+
+if result:
+       
+    print("Processing assets to find buy signals")
+    st.text("Processing assets to find buy signals")
+    
+    progress_bar = st.progress(0)
+    
+    status_text = st.empty()
+    
+    for i in range(totalfields):  
+        # Update progress bar.
+        progess = (i * 100 / totalfields) / 100
+        print(progess)
+        progress_bar.progress(progess)
         
-        print(toBuy)
         
-matrixsignalsbyAsset = dict(zip(tickers, matrixsignals))  
+        
+        frame = RSIcalc(tickers[i])
+        buy, sell = getSignals(frame)
+        Profits = (frame.loc[sell].Open.values - frame.loc[buy].Open.values)/frame.loc[buy].Open.values
+        matrixsignals.append(buy)
+        matrixprofits.append(Profits)
+        
+        #Date range to check if there is a signal to buy the asset
+        dateTo = datetime.datetime.today()
+        dateFrom = dateTo + datetime.timedelta(days=-60)
+        
+        dfBuy = frame.loc[dateFrom:dateTo, ['Buy']]
+        toBuy = dfBuy[dfBuy['Buy']=='Yes']
+        toBuy['Ticker'] = tickers[i]
+        
+        print('Processed: ' + str(counter) + ' of ' + str(totalfields))
+        
+        # Update status text.
+        status_text.text(
+            'Processed: ' + str(counter) + ' of ' + str(totalfields))
+        
+        counter = counter + 1
+        
+        #In case we have a signal to buy in the specified period, let's plot it
+        if not toBuy.empty:
+            print('Asset to buy: ')
+            
+            plt.figure(figsize=(12,5))
+            plt.scatter(frame.loc[buy].index, frame.loc[buy]['Adj Close'], marker='^', c='g')
+            plt.plot(frame['Adj Close'], alpha=0.7)
+            plt.title(label=tickers[i])
+            
+            st.pyplot(plt)
+            
+            st.write(toBuy)
+            #st.line_chart(frame['Adj Close'])
+    
+            #st.plotly_chart(plt)
+            
+            print(toBuy)
+            
+#matrixsignalsbyAsset = dict(zip(tickers, matrixsignals))  
 
 
 
